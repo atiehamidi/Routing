53061e83624b9fbfc5d5877e20b399e5184d64ee
Atieh Hamidi <atieh@MacBook-Pro.local>
2020-07-03 09:54:39 +0200

    declare Route for Nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Router, Link, NavLink, NavBar } from "react-router-dom";
+import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">AboutPage</NavLink>
+        <NavLink to="/discover">DiscoverMoviesPage</NavLink>
+      </main>
+
+      <Route path="/discover" component={DiscoverMoviesPage} />
+      <Route path="/about/:me" component={AboutPage} />
+      <Route exact path="/" component={HomePage} />
     </div>
   );
 }
-
-export default App;
